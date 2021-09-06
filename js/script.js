fd6ecff8efd8e5ba9301f7135c60a0bc9de00f83
Gavin-Kariuki <gavin.kariuki@student.moringaschool.com>
2021-09-06 12:43:48 +0300

    add comments for future reference

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const formSubmit = (event) => {
     const formData = new FormData(frm)
     const date = new Date(formData.get('bday'))
     const Year = date.getFullYear().toString()
-    const CC = parseInt(Year.slice(0,2))
+    const CC = parseInt(Year.slice(0,2)) // parseInt returns an integer //
     const YY = parseInt(Year.slice(2,4));
     const MM = date.getMonth()
     const DD = date.getDay()
